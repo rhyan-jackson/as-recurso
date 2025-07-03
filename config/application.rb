82c859c2136a52e437aaa7fdbdfd924d3bf295f1
Rhyan Matos <rhyanjacksonmarinho@gmail.com>
2025-07-03 21:39:17 +0100

    fix: set timezone to Lisbon

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module EBuga
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Lisbon"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
