25d978830800db9233282a41b9f46a462c48f64f
penachoUA <penacho@ua.pt>
2025-07-04 08:24:29 +0100

    Fix bug with onboarding

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  # residente@exemplo.com
+  # 123456
 end
